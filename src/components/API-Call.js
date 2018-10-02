c6feadd4c5b17056b46c074b1c1eaee31a4c6bf7
HyrGlobalSystem <sasidhareee.reddy@gmail.comn>
2018-10-02 11:56:36 -0500

    nothing changed

--- src/components/API-Call.js
+++ src/components/API-Call.js
@@ -5,36 +5,33 @@ import React from 'react';
 
 
 class Axioscall extends React.Component{
-        //  state={
-        //   user:[],
-        //   invalid:''
-        // }
-        componentDidMount(){
-            handleAPI=(e)=>{
-                e.preventDefault();
-                axios.get(`https://api.github.com/users/${this.props.userName}`)
-                .then(res=>{
-                  const user=res.data;
-                  //this.setState({user});
-                }).catch(error=>{
-                    const invalid=error.response.statusText
-                    //this.setState({invalid});
-                });
-                    this.invalid='';
-                    this.user=[];
-               };
-        
+         state={
+          user:[],
+          invalid:''
         }
-       
+        handleAPI=(e)=>{
+        e.preventDefault();
+        axios.get(`https://api.github.com/users/${this.props.userName}`)
+        .then(res=>{
+          const user=res.data;
+          this.setState({user});
+        }).catch(error=>{
+            const invalid=error.response.statusText
+            this.setState({invalid});
+        });
+            this.state.invalid='';
+            this.state.user=[];
+       };
+
 render(){
     return(
         
         <div>
         <button className="button" onClick={this.handleAPI}>search</button>
-        <h4>{this.invalid}</h4>
-        <h4>Name: {this.user.name}</h4>
-        <h4>UserName: {this.user.login}</h4>
-        <h4>UserUrl: <a href={this.user.html_url}>{this.user.html_url}</a></h4>
+        <h4>{this.state.invalid}</h4>
+        <h4>Name: {this.state.user.name}</h4>
+        <h4>UserName: {this.state.user.login}</h4>
+        <h4>UserUrl: <a href={this.state.user.html_url}>{this.state.user.html_url}</a></h4>
         </div>
     )
 }
